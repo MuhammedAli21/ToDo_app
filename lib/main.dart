ebd042e105570fdb09e4da4056fab8c2b9899bb7
MuhammedAli21 <mohamedmohamed20211@gmail.com>
2024-07-21 21:33:42 +0300

    init project.

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/screens/home_screen.dart';
+
+void main(){
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      showSemanticsDebugger: false,
+      initialRoute: HomeScreen.routName ,
+      routes: {
+        HomeScreen.routName : (context) => HomeScreen(),
+      },
+    );
+  }
+}
